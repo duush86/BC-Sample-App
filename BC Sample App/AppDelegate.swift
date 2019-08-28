1c5a8095a6794902680ab0db43ae067f0c27d22a
Antonio <j.antonio.oro@gmail.com>
2019-08-28 15:00:01 -0500

    Basic Brightcove Player implementation

--- BC Sample App/AppDelegate.swift
+++ BC Sample App/AppDelegate.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import AVFoundation
+
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +18,19 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        var categoryError :NSError?
+        var success: Bool
+        do {
+            try AVAudioSession.sharedInstance().setCategory(.playback, mode: .moviePlayback, options: .duckOthers)
+            success = true
+        } catch let error as NSError {
+            categoryError = error
+            success = false
+        }
+        
+        if !success {
+            print("AppDelegate Debug - Error setting AVAudioSession category.  Because of this, there may be no sound. \(categoryError!)")
+        }
         return true
     }
 
--- BC Sample App/Controller/VideoViewController.swift
+++ BC Sample App/Controller/VideoViewController.swift
@@ -7,35 +7,75 @@
 //
 
 import UIKit
+import BrightcovePlayerSDK
 
-class VideoViewController: UIViewController {
-    var selectedDemo: Demo? {
-        didSet{
-            loadItem()
-        }
+let kViewControllerPlaybackServicePolicyKey = "BCpkADawqM32nL1Ic9gyo3bITy-1QWVkCxdmpEw9LLw3BrW7TwxPPCaWEq5OoIRzx9E3ydeeS2uir3OOi2ziy2Dh5NjlAqavWfSjyFXkTtHB69KQkyc0-FAXel3bqWzTFdMuFXy0RjhXsecd"
+let kViewControllerAccountID = "6030890615001"
+
+class VideoViewController: UIViewController, BCOVPlaybackControllerDelegate {
+    var selectedDemo: Demo?
+  
+    @IBOutlet weak var videoContainerView: UIView!
+    
+    let sharedSDKManager = BCOVPlayerSDKManager.shared()
+    let playbackService = BCOVPlaybackService(accountId: kViewControllerAccountID, policyKey: kViewControllerPlaybackServicePolicyKey)!
+    let playbackController :BCOVPlaybackController
+    
+    required init?(coder aDecoder: NSCoder) {
+        playbackController = (sharedSDKManager?.createPlaybackController())!
+        
+        super.init(coder: aDecoder)
+        
+        playbackController.analytics.account = kViewControllerAccountID // Optional
+        
+        playbackController.delegate = self
+        playbackController.isAutoAdvance = true
+        playbackController.isAutoPlay = true
     }
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-        //navigationItem.backBarButtonItem = VideoViewController
+        
+       
+        loadVideo()
         
     }
+    
+    func requestContentFromPlaybackService() {
+        playbackService.findVideo(withVideoID: selectedDemo?.content_id, parameters: nil) { (video: BCOVVideo?, jsonResponse: [AnyHashable: Any]?, error: Error?) -> Void in
+            
+            if let v = video {
+                self.playbackController.setVideos([v] as NSArray)
+            } else {
+                print("ViewController Debug - Error retrieving video: \(error?.localizedDescription ?? "unknown error")")
+            }
+        }
+    }
+    
+    
     override func viewWillAppear(_ animated: Bool) {
         title = selectedDemo?.name
     }
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-        func loadItem(){
         
+    
+    func loadVideo(){
+            
+            guard let playerView = BCOVPUIPlayerView(playbackController: self.playbackController, options: nil, controlsView: BCOVPUIBasicControlView.withVODLayout()) else {
+                return
+            }
+            self.videoContainerView.addSubview(playerView)
+            playerView.translatesAutoresizingMaskIntoConstraints = false
+            NSLayoutConstraint.activate([
+                playerView.topAnchor.constraint(equalTo: self.videoContainerView.topAnchor),
+                playerView.rightAnchor.constraint(equalTo: self.videoContainerView.rightAnchor),
+                playerView.leftAnchor.constraint(equalTo: self.videoContainerView.leftAnchor),
+                playerView.bottomAnchor.constraint(equalTo: self.videoContainerView.bottomAnchor)
+                ])
+            
+            playerView.playbackController = playbackController
+            
+            requestContentFromPlaybackService()
         }
 }
